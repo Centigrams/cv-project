2e1ab51c05621eb42767b10362d3a530f25dd41a
Sidd Bairan <bairansa@gmail.com>
2022-02-01 16:19:44 +0800

    Finish work experience section

--- src/Components/ResumePreview.js
+++ src/Components/ResumePreview.js
@@ -4,18 +4,35 @@ import PersonalInfo from './PersonalInfo';
 import WorkExperienceSection from './WorkExperienceSection';
 import EducationSection from './EducationSection';
 import SkillsInterestsSection from './SkillsInterestsSection';
-import { EditModeContext } from './MainContent';
+import { EditModeContext, WorkExperienceContext, WorkExperiencesContext } from './MainContent';
 import SectionHeaderButton from './SectionHeaderButton';
 
 // TODO: Add element functionality.
 
 function ResumePreview() {
   const { editMode } = useContext(EditModeContext);
+  const { workExperience, setWorkExperience } = useContext(WorkExperienceContext);
+  const { workExperiencesArray, setWorkExperiencesArray } = useContext(WorkExperiencesContext);
+
   let workExperienceHeader;
   let educationHeader;
   if (editMode) {
-    workExperienceHeader = <SectionHeaderButton text="ADD WORK EXPERIENCE" />;
-    educationHeader = <SectionHeaderButton text="ADD EDUCATION" />;
+    workExperienceHeader = (
+      <SectionHeaderButton
+        text="ADD WORK EXPERIENCE"
+        section="workExperience"
+        workExperience={workExperience}
+        setWorkExperience={setWorkExperience}
+        workExperiencesArray={workExperiencesArray}
+        setWorkExperiencesArray={setWorkExperiencesArray}
+      />
+    );
+    educationHeader = (
+      <SectionHeaderButton
+        text="ADD EDUCATION"
+        section="education"
+      />
+    );
   } else {
     workExperienceHeader = <h2 className={resumePreviewStyles.header}>WORK EXPERIENCE</h2>;
     educationHeader = <h2 className={resumePreviewStyles.header}>EDUCATION</h2>;
--- src/Components/SectionHeaderButton.js
+++ src/Components/SectionHeaderButton.js
@@ -1,13 +1,47 @@
 import React from 'react';
 import AddIcon from '@mui/icons-material/Add';
+import { v4 as uuidv4 } from 'uuid';
 import CustomAddButtons from '../Styles/MUICustomStyledComponents/CustomAddButtons';
 
-function SectionHeaderButton({ text }) {
-  return (
-    <CustomAddButtons variant="outlined" endIcon={<AddIcon />}>
-      {text}
-    </CustomAddButtons>
-  );
+function SectionHeaderButton({
+  text,
+  section,
+  workExperience,
+  setWorkExperience,
+  workExperiencesArray,
+  setWorkExperiencesArray,
+}) {
+  const addNewWorkExperience = () => {
+    setWorkExperiencesArray(workExperiencesArray.concat(workExperience));
+    setWorkExperience({
+      id: uuidv4(),
+      companyName: 'Company Name',
+      jobTitle: 'Job Title',
+      tenure: 'Month Year - Month Year',
+      location: 'City, ST',
+      firstCR: 'Core Responsibility',
+      secondCR: 'Core Responsibility',
+      thirdCR: 'Core Responsibility',
+    });
+  };
+
+  // TODO: Add education functionality.
+  const addEducation = () => {
+  };
+
+  if (section === 'workExperience') {
+    return (
+      <CustomAddButtons variant="outlined" endIcon={<AddIcon />} onClick={addNewWorkExperience}>
+        {text}
+      </CustomAddButtons>
+    );
+  } if (section === 'education') {
+    return (
+      <CustomAddButtons variant="outlined" endIcon={<AddIcon />} onClick={addEducation}>
+        {text}
+      </CustomAddButtons>
+    );
+  }
 }
 
 export default SectionHeaderButton;
